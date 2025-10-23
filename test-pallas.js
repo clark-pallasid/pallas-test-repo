6d794fdbd7fbaf9f1faa51a5000e50cfbc506606
clark-pallasid <clark@pallasid.com>
2025-10-23 12:20:26 -0700

    Add test file for Pallas GitHub tools validation

---
+++ test-pallas.js
@@ -0,0 +1,8 @@
+// Testing Pallas GitHub tools
+console.log("Hello from Pallas testing!");
+
+function testPallasIntegration() {
+    return "GitHub tools working correctly!";
+}
+
+module.exports = { testPallasIntegration };
